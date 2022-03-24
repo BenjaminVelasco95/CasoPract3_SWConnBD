c5a3c286bc8c85e0d0b84eb0d1ed78a8dd49e8b8
BenjaminVelasco95 <A170135@unach.mx>
2022-03-23 19:09:32 -0600

    Corrección en la nomenclatura de los webmethod

--- CasoPract3_SWConnBD/Servicios/ClientesWebService.asmx.cs
+++ CasoPract3_SWConnBD/Servicios/ClientesWebService.asmx.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Services;
+﻿using System.Web.Services;
 using MySql.Data.MySqlClient;
-using System.Data;
 
 namespace CasoPract3_SWConnBD
 {
 
     /// <summary>
     /// Descripción breve de ClientesWebService
+    /// CRUD de libros a travez de un servcicio web-SOAP
     /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
@@ -19,18 +15,20 @@ namespace CasoPract3_SWConnBD
     // [System.Web.Script.Services.ScriptService]
     public class ClientesWebService : System.Web.Services.WebService
     {
+        #region conexion
         static string connString = "server=localhost;port=3306;user id=root;password=;database=pruebaws;";
         MySqlConnection conn = new MySqlConnection(connString);
         MySqlCommand cmd = new MySqlCommand();
         MySqlDataReader reader = null;
-
-        [WebMethod]
-        public string HelloWorld()
-        {
-            return "Hola a todos";
-        }
-        [WebMethod(Description = "Inserta Datos en la tabla Libro")]
-        public string AddCliente(string nombre, string autor, string editorial, string pais, string fecha)
+        #endregion
+        /*
+         * 
+         * CRUD de usuarios.
+         * 
+         * 
+         * */
+        [WebMethod(Description = "Inserta un registro en la tabla Libros")]
+        public string AddLibro(string nombre, string autor, string editorial, string pais, string fecha)
         {
             try
             {
@@ -45,8 +43,8 @@ namespace CasoPract3_SWConnBD
             }
             return "true";
         }
-        [WebMethod (Description ="Edita los datos de un Libro")]
-        public string ModCliente(string idLibro, string nombre, string autor, string editorial, string pais, string fecha)
+        [WebMethod (Description ="Edita un registro de la tabla Libros")]
+        public string ModLibro(string idLibro, string nombre, string autor, string editorial, string pais, string fecha)
         {
             try
             {
@@ -58,12 +56,12 @@ namespace CasoPract3_SWConnBD
             }
             catch(MySqlException e)
             {
-                return e.ToString();
+                return "error! " + e.ToString();
             }
             return "true";
         }
-        [WebMethod(Description ="Elimina los datos de un Libro")]
-        public string DellCliente(string idLibro)
+        [WebMethod(Description ="Elimina un registro de la tabla Libros")]
+        public string DellLibro(string idLibro)
         {
             try
             {
